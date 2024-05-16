4fca0226b727f1bb3f0e01626165c3f018b02375
Nicko Satria Utama <nicko.su@hotmail.com>
2024-05-16 14:29:05 +0800

    change about company

--- components/TimelineDevelopment.vue
+++ components/TimelineDevelopment.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="main-container">
         <div>
-            <h1>Our APP development timeline</h1>
-            <p>Our MVP app will deploy in mobile phone with AI based recommendation and automation</p>
+            <h1>We will use and develop our own marketplace</h1>
+            <p>We will develop e-commerce for our selling products in the web application and mobile application</p>
+            <p>Our priorrity is B2B customer with bulk buyer.</p>
             <p>Below is our plan for app development until 2025 (2 year action plan)</p>
+            <p>We will split the stores from B2B customers and B2C customers</p>
         </div>
         <div class="intro-section overlay bg-cover" :style="{
             backgroundImage: `url('/images/bg/javaraya-app-timeline.png')`,
--- pages/about/tandc.vue
+++ pages/about/tandc.vue
@@ -4,6 +4,7 @@
     <SearchPopup />
     <BreadcrumbOne activePageName="Term and Condition" title="Term and Condition" backgroundUrl= "/images/bg/breadcrumb-bg-darkgray.png" />
    
+    
 
 
     <TheFooter />
